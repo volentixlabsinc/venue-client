0d26659b6349be7358998e2d9d08330b4b185101
Smouska <sandragjacinto@gmail.com>
2018-04-28 12:54:58 -0400

    adds homepage view

---
+++ src/views/home/logIn.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+  <h1>Welcome to Venue</h1>
+        <button 
+        @click="$router.push('/dashboard')"
+        class="btn venue-accent-color">log in</button>
+        </div>
+</template>
