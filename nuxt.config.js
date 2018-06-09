e300c25bf41c114bef22d564c67f6393b8654afb
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-06-08 22:35:09 -0400

    Import fontawesome stylesheet

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,12 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'A rewards and bounty tracker platform' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', 
+        href: 'https://use.fontawesome.com/releases/v5.0.13/css/all.css', 
+        integrity: 'sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp',
+        crossorigin: 'anonymous'
+      }
     ]
   },
   /*
