79c7eaf0242507fc3bd8594c010870e0086a3e6a
Smouska <sandragjacinto@gmail.com>
2018-06-12 15:23:56 -0400

    moifications to handle conflicts

--- components/AvailableSignatures.vue
+++ components/AvailableSignatures.vue
@@ -2,43 +2,15 @@
   <div>
      <form>
        <ul>
-          <li>
+          <li v-for="sig in signatures" :key="sig.id">
               <div>
-                <img src="/img/onboarding/sig1.png" />
-                <button type="button" v-clipboard:copy="signatures[0].code"
-                    v-clipboard:success="() => onCopy(signatures[0])">COPY CODE</button>
-             </div>
-          </li>
-          <li>
-              <div>
-                <img src="/img/onboarding/sig2.png" />
-                <button type="button" v-clipboard:copy="signatures[1].code"
-                    v-clipboard:success="() => onCopy(signatures[1])">COPY CODE</button>
-             </div>
-          </li>
-          <li>
-              <div>
-                <img src="/img/onboarding/sig3.png" />
-                <button type="button" v-clipboard:copy="signatures[2].code"
-                    v-clipboard:success="() => onCopy(signatures[2])">COPY CODE</button>
-             </div>
-          </li>
-          <li>
-              <div>
-                <img src="/img/onboarding/sig4.png" />
-                <button type="button" v-clipboard:copy="signatures[3].code"
-                    v-clipboard:success="() => onCopy(signatures[3])">COPY CODE</button>
-             </div>
-          </li>
-          <li>
-              <div>
-                <img src="/img/onboarding/sig5.png" />
-                <button type="button" v-clipboard:copy="signatures[4].code"
-                    v-clipboard:success="() => onCopy(signatures[4])">COPY CODE</button>
+                <img :src="sig.image" />
+                <button type="button" v-clipboard:copy="sig.code"
+                    v-clipboard:success="() => onCopy(sig)">COPY CODE</button>
              </div>
           </li>
         </ul>
-      </form>
+      </form>  
       <ModalWidget v-if="showModal" @close="showModal = false" :request="'confirmCopy'"/>
   </div>
 </template>
@@ -50,26 +22,35 @@ import copiedSignature from '~/components/copiedSignature.vue'
 
     const BITCOINTALK_FORUM_ID = 1
     
-    export default {
-        components: {
-            ModalWidget,
-            copiedSignature
-        },
-        data() {
-            return {
-            signatures: [{"id":9,"name":"BitcoinTalk Test Signature 7","image":"/img/onboarding/sig1.png","code":"Men never remember, but women never forget","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":8,"name":"BitcoinTalk Test Signature 6","image":"/img/onboarding/sig2.png","code":"A friend walks in when everyone else walks out","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":7,"name":"BitcoinTalk Test Signature 5","image":"/img/onboarding/sig3.png","code":"Follow love and it will flee, flee love and it will follow","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":6,"name":"BitcoinTalk Test Signature 4","image":"/img/onboarding/sig4.png","code":"In teaching others we teach ourselves","verification_code":"eRycp9ln","usage_count":1,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":5,"name":"BitcoinTalk Test Signature 3","image":"","code":"Knowledge comes, but wisdom lingers","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":3,"name":"BitcoinTalk Test Signature 2","image":"/img/onboarding/sig6.png","code":"When in Rome, do as the Romans","verification_code":"eRycp9ln","usage_count":2,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":1,"name":"BitcoinTalk Test Signature 1","image":"/img/onboarding/sig7.png","code":"The pen is mightier than the sword","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"}],
-            showModal: false
-            }
-        },
-        methods: {
-            onCopy: function (sig) {
-                this.$store.commit('signatureCopied', sig.id)
-                this.showModal = true;
-            },
-          
+export default {
+    // props: {
+    //     signatures: {
+    //         type: Array,
+    //         default: [{"id":9,"name":"BitcoinTalk Test Signature 7","image":"/img/onboarding/sig1.png","code":"Men never remember, but women never forget","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":8,"name":"BitcoinTalk Test Signature 6","image":"/img/onboarding/sig2.png","code":"A friend walks in when everyone else walks out","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":7,"name":"BitcoinTalk Test Signature 5","image":"/img/onboarding/sig3.png","code":"Follow love and it will flee, flee love and it will follow","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":6,"name":"BitcoinTalk Test Signature 4","image":"/img/onboarding/sig4.png","code":"In teaching others we teach ourselves","verification_code":"eRycp9ln","usage_count":1,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":5,"name":"BitcoinTalk Test Signature 3","image":"","code":"Knowledge comes, but wisdom lingers","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":3,"name":"BitcoinTalk Test Signature 2","image":"/img/onboarding/sig6.png","code":"When in Rome, do as the Romans","verification_code":"eRycp9ln","usage_count":2,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":1,"name":"BitcoinTalk Test Signature 1","image":"/img/onboarding/sig7.png","code":"The pen is mightier than the sword","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"}]
+    //     }
+    // },
+    components: {
+        ModalWidget,
+        copiedSignature
+    },
+    data() {
+        return {
+        signatures: [{"id":9,"name":"BitcoinTalk Test Signature 7","image":"/img/onboarding/sig1.png","code":"Men never remember, but women never forget","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":8,"name":"BitcoinTalk Test Signature 6","image":"/img/onboarding/sig2.png","code":"A friend walks in when everyone else walks out","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":7,"name":"BitcoinTalk Test Signature 5","image":"/img/onboarding/sig3.png","code":"Follow love and it will flee, flee love and it will follow","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":6,"name":"BitcoinTalk Test Signature 4","image":"/img/onboarding/sig4.png","code":"In teaching others we teach ourselves","verification_code":"eRycp9ln","usage_count":1,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":5,"name":"BitcoinTalk Test Signature 3","image":"","code":"Knowledge comes, but wisdom lingers","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":3,"name":"BitcoinTalk Test Signature 2","image":"/img/onboarding/sig6.png","code":"When in Rome, do as the Romans","verification_code":"eRycp9ln","usage_count":2,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"},{"id":1,"name":"BitcoinTalk Test Signature 1","image":"/img/onboarding/sig7.png","code":"The pen is mightier than the sword","verification_code":"eRycp9ln","usage_count":0,"forum_site_name":"bitcointalk.org","forum_user_name":"dannyl","forum_userid":"2093382"}],
+        showModal: false
+        }
+    },
+    methods: {
+        onCopy: function (sig) {
+            this.$store.commit('signatureCopied', sig.id)
+            this.showModal = true;
         },
         
+    },
+    created () {
+        console.log('created', this.signatures)
     }
+    
+}
 </script>
 
 <style scoped>
