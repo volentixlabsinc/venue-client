98b7f61abe825b1b61b8a1835378b92e650cc0e1
Smouska <sandragjacinto@gmail.com>
2018-06-12 13:37:07 -0400

    adds image for helpsignature paste

--- components/HelpSignatureImages.vue
+++ components/HelpSignatureImages.vue
@@ -1,41 +1,159 @@
 <template>
-    <div>
-        <transition name="fader">
-            <img v-if="showstep" src='/img/onboarding/bitcointalk/step1a_alt.png' />
-            <img v-if="!showstep" src='/img/onboarding/bitcointalk/step1b_alt.png' />
-        </transition>
+    <div class="main-modal">
+        <div class="steps-section">
+            <button class="steps-buttons" @click="imageNumber=1">1</button>
+            <hr width="30%" :style="[imageNumber>=2 ? {'border-color':'#DD9C3F'} : {'border-color':'#cab595'}]">
+            <button class="steps-buttons" 
+            @click="imageNumber=2" 
+            :style="[imageNumber>=2 ? {'background-color':'#DD9C3F'} : {'background-color':'#cab595', 'border-color':'#cab595', 'box-shadow':'none'}]">2</button>
+            <hr width="30%" :style="[imageNumber>=3 ? {'border-color':'#DD9C3F'} : {'border-color':'#cab595'}]">
+            <button class="steps-buttons" @click="imageNumber=3"
+            :style="[imageNumber>=3 ? {'background-color':'#DD9C3F'} : {'background-color':'#cab595', 'border-color':'#cab595', 'box-shadow':'none'}]">3</button>
+        </div>
+        <div class="tips-section">
+            <div v-if="imageNumber==1" class="userId-form">
+            <h3>1) Click on Forum Profile Information inside the Profile Tab</h3>
+            <img src='/img/onboarding/bitcointalk/sigPaste1.png' />
+            </div>
+            <div v-if="imageNumber==2" class="userId-form">
+            <h3>2) Scroll down to Signature and paste the code in the Text Area</h3>
+            <img v-if="imageNumber==2" id="signature-container" src='/img/onboarding/bitcointalk/sigPaste2.png' />
+            </div>
+            
+            <div v-if="imageNumber==3" class="userId-form">
+                <h3>3) Scroll down and accept the changes</h3>
+                <img v-if="imageNumber==3" src='/img/onboarding/bitcointalk/sigPaste3.png' />
+                <div class="md-form">
+                    <button class="btn venue-accent-color" @click="$emit('close')">Done</button>
+                </div>
+            </div>
+        </div>
+
+        <button v-if="imageNumber<3" class="btn btn-small venue-accent-color modal-default-button" @click="imageNumber+=1">
+            NEXT
+        </button>
     </div>
 </template>
 
 <script>
+
+import {
+  checkProfile,
+  createForumProfile
+} from "~/services/signatures";
+ 
+
 export default {
-   
+    data() {
+        return({
+            imageNumber: 1,
+            showstep: true,
+            userId: null,
+            BITCOINTALK_FORUM_ID: 1
+        })
+    },
+    methods: {
+            
+        fetchCreateForumProfile() {
+            const scope = this;
+        createForumProfile (scope.userId, BITCOINTALK_FORUM_ID, true) 
+        .then(response => {
+            scope.$store.commit('forums/register', {
+            forumId: BITCOINTALK_FORUM_ID,
+            forumUserId: response.forum_user_id,
+            venueForumUserId: response.forum_profile_id
+          })
+        })
+      }
+    },
+    mounted() {
+        // setInterval(this.swap, 4000);
+    }
 }
 </script>
 
 <style scoped>
-.fader-enter {
-    opacity: 0;
-    animation: f 1s;
+hr {
+    border: 1px solid gray;
 }
-.fader-leave-active {
-    opacity: 0;
-    animation: f 1s reverse;
+
+button:focus {outline:0;}
+.steps-section {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 }
-.fader-enter,
-.fader-leave-active {
-    opacity: 100;
+
+
+.steps-buttons {
+    font-weight: bold;
+    font-size: 14px;
+    border-radius: 50%;
+    height: 50px;
+    width: 50px;
+    background-color: #DD9C3F;
+    border: 1px solid #DD9C3F;
+    color: white;
+    box-shadow: 1px 0.5px 8px rgba(0, 0, 0, 0.312);
 }
-@keyframes f {
-    75% {
-        opacity: 100%;
-    }
+
+.main-modal{
+    color: white;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
 }
+
+.tips-section{
+    width: 100%;
+    height: 70%;
+}
+
 img {
-    max-width: 525px;
+    max-height: 90%;
     border-radius: 5px;
 }
-@media only screen and (max-width: 600px) {
+#signature-container{
+    width: 100%;
+}
+.userId-form{
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.md-form{
+    width: 100%;
+    margin:0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+
+.form-control{
+  width: 50% !important;
+  height: 25px;
+  border-radius: 0px;
+  background-color: rgba(255, 254, 254, 0.1);
+  border: none;
+  border-bottom: 1px solid white;
+  color: white;
+  font-size: 14px;
+  margin: 5px !important;
+}
+
+::placeholder{
+    color: white;
+    padding-left: 5px;
+}
+.form-control:focus {outline:0;}
+@media only screen and (max-width: 800px) {
     .img {
         width: 95%;
     }
