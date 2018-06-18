35a5c1ff57c8fc2f95ccc6c75b509557b4011f67
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-06-18 17:35:45 +0100

    Use columns instead of tiles for profile

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -2,49 +2,19 @@
   <one-column-layout>
     <div>
       <h1 class="title is-2">MY PROFILE</h1>
-      <div v-if="userInfo" class="tile is-ancestor is-multiline">
-        <div class="tile">
-          <wallet-address />
-        </div>
-
-        <div class="tile">
-          <email-address :user-info="userInfo.email" @changeEmail="$modal.show('MyEmailModal')"/>
-        </div>
-
-        <div class="tile">
-          <account-username :user-info="userInfo.username" @newUserData="refreshData"/>
-        </div>
-      </div>
-      <div v-if="userInfo" class="tile is-ancestor is-multiline">
-        <div class="tile">
+      <div v-if="userInfo" class="columns">
+        <div class="column">
+          <wallet-address/>
           <account-password/>
         </div>
-
-        <div class="tile">
-          <language :user-info="userInfo.language" :languages="languages"/>
+        <div class="column">
+          <email-address :user-info="userInfo.email" @changeEmail="$modal.show('MyEmailModal')"/>
+          <language />
         </div>
-
-        <div class="tile">
+        <div class="column">
+          <account-username :user-info="userInfo.username" @newUserData="refreshData"/>
           <two-factor/>
         </div>
-      </div>
-    
-      <div v-show="showError.error" class="columns is-desktop is-vcentered has-text-centered" style="width:100%; height:100%">
-        <section class="hero">
-          <div class="hero-body">
-            <div class="container">
-              <h1 class="title ">
-                Ooops, Bad Request! 
-              </h1>
-              <!-- <h2 class="subtitle">
-              {{ showError.message.stringify() }}
-            </h2> -->
-              <h2 class="subtitle">
-                To have access to your profile you must be logged in.
-              </h2>
-            </div>
-          </div>
-        </section>
       </div>
     </div>
   </one-column-layout>
@@ -72,43 +42,12 @@ export default {
   },
   data() {
     return {
-      request: null,
-      userInfo: null,
-      languages: null,
-      showError: false
+      userInfo: null
     };
   },
-  mounted() {
-    this.retrieveUserInfo();
-    this.fetchLanguages();
-  },
-  methods: {
-    async retrieveUserInfo() {
-      await this.$axios
-        .$get("/retrieve/user/")
-        .then(result => {
-          this.userInfo = result;
-        })
-        .catch(error => {
-          this.showError = {
-            error: true,
-            message: error
-          };
-        });
-    },
-    async fetchLanguages() {
-      const getLanguages = await this.$axios.$get("/retrieve/languages/");
-      this.languages = getLanguages;
-    },
-    refreshData(data) {
-      this.userInfo = data;
-    }
+  async asyncData({ app }) {
+    const userInfo = await app.$axios.$get("/retrieve/user/");
+    return { userInfo };
   }
 };
 </script>
-
-<style scoped>
-.tile {
-  margin: 3em 1em;
-}
-</style>
