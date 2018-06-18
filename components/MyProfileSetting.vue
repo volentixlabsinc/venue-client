c36d596ed6c237a8a3374ad7d12fb1c92ef5f189
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-06-18 16:32:47 +0100

    Use a component for MyProfileSetting

---
+++ components/MyProfileSetting.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="panel">
+    <h2 class="panel-heading">{{ title }}</h2>
+    <div id="content" class="panel-block">
+      <div>
+        <div>{{ description }}</div>
+        <span v-if="comingSoon" class="tag is-warning m-t-md">Coming Soon</span>
+      </div>
+    </div>
+     
+    <div class="panel-block">
+      <a :href="href" :disabled="comingSoon" class="button is-fullwidth is-primary">{{ button }}</a>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    description: {
+      type: String,
+      default: ""
+    },
+    button: {
+      type: String,
+      default: ""
+    },
+    href: {
+      type: String,
+      default: ""
+    },
+    comingSoon: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style>
+#content {
+  height: 8em;
+}
+</style>
--- components/myProfile/WalletAddress.vue
+++ components/myProfile/WalletAddress.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="card">
-    <div class="card-content">
-      <h2 class="title"> VTX Wallet Address</h2>
-      <h2 class="subtitle"> VTX address whete to send payouts</h2>
-      <footer class="card-footer">
-        <div class="card-footer-item">
-          <a href="#" class="button is-primary is-grouped-multiline" disabled>Set VTX Adress <label class="tag is-warning is-small">Coming Soon</label></a>
-        </div>
-      </footer>
-    </div>
-  </div>
+  <my-profile-setting :coming-soon="true" title="VTX Wallet Address" description="VTX address where to send payouts" button="Set VTX Address" href="#" />
+
 </template>
+
+<script>
+import MyProfileSetting from "~/components/MyProfileSetting.vue";
+
+export default {
+  components: {
+    MyProfileSetting
+  }
+};
+</script>
