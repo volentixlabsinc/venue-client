4c0d5290386d538ca67929f008d6132dcbe0ff5b
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-06-18 16:43:46 +0100

    Fix 2FA text

--- components/myProfile/TwoFactor.vue
+++ components/myProfile/TwoFactor.vue
@@ -2,10 +2,10 @@
   <div class="card">
     <div class="card-content">
       <h2 class="title"> Two-Factor Authentication</h2>
-      <h2 class="subtitle">Disabled two-factor authentication</h2>
+      <h2 class="subtitle">Modify two-factor authentication</h2>
       <footer class="card-footer">
         <div class="card-footer-item">
-          <a href="#" class="button is-primary is-grouped-multiline" disabled>Disable 2FA <label class="tag is-warning is-small">Coming Soon</label></a>
+          <a href="#" class="button is-primary is-grouped-multiline" disabled>Enable 2FA <label class="tag is-warning is-small">Coming Soon</label></a>
         </div>
       </footer>
     </div>
