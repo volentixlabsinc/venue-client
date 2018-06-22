c22e7a11aa712cbae9799c85f6333cb3255e090e
Smouska <sandragjacinto@gmail.com>
2018-06-22 15:08:57 -0400

    adds enbale flow

--- components/myProfile/enable2FA.vue
+++ components/myProfile/enable2FA.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>enable</h1>
+  <div>
+    <h1>enable</h1>
+    <h2 class="subtitile">{{ bodyText[0] }}</h2>
+    <qr-code :text="uri" :size="220"/>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    bodyText: {
+      type: Array,
+      default: null
+    }
+  },
+  data() {
+    return {
+      uri: undefined
+    };
+  },
+  mounted() {
+    this.fetchRequest();
+  },
+  methods: {
+    async fetchRequest() {
+      const response = await this.$axios.$post(
+        "/manage/enable-two-factor-auth"
+      );
+      this.uri = response.uri;
+    }
+  }
+};
+</script>
