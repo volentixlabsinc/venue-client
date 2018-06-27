1360f7c40422a8aeb08f2b46c0314d421032eea4
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-06-27 16:08:22 -0400

    Fix navigation on mobile

--- venue/components/NavigationLink.vue
+++ venue/components/NavigationLink.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="menu-item menu-link m-md">
-    <router-link :to="route">
-      <div class="level">
-        <span class="icon is-large level-left"><i :class="icon" class="fa-2x"/></span>
-        <span class="is-size-6 is-pulled-left level-right">{{ name }}</span>
-      </div>
-    </router-link>
-  </div>
+  <router-link :to="route">
+    <div class="navbar-item level">
+      <span class="icon is-large"><i :class="icon" class="fas fa-lg prefix"/></span>
+      <span class="is-size-6">{{ name }}</span>
+    </div>
+  </router-link>
 </template>
 
 <script>
