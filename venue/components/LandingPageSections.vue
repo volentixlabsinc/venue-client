02395b9d743d74d8ae1b6917b90fbd49c68ed520
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-06-29 22:49:11 -0400

    Properly fix incorrect shading in Landing Page

--- venue/components/LandingPageSections.vue
+++ venue/components/LandingPageSections.vue
@@ -2,15 +2,13 @@
   <section class="section">
     <h3 class="title is-3">{{ title }}</h3>
     <div class="level">
-      <div v-for="(elements, index) in content" :key="index" class="card has-background-light" style="width:24%!important; border:1px solid grey">
-        <div class="card">
-          <div class="card-content">
-            <div :class="elements.disabled ? disabled : active" class="is-size-3" >{{ elements.text }}</div>
-            <a v-show="!(isAuthenticated && !elements.disabled)"
-               :disabled="elements.disabled" 
-               :class="elements.disabled ? disabled : active" class="is-pulled-right" 
-               @click="onclickJoin(elements.disabled)">{{ elements.linkText }}</a>
-          </div>
+      <div v-for="(elements, index) in content" :key="index" class="card" style="width:24%!important; border:1px solid grey">
+        <div class="card-content">
+          <div :class="elements.disabled ? disabled : active" class="is-size-3" >{{ elements.text }}</div>
+          <a v-show="!(isAuthenticated && !elements.disabled)"
+             :disabled="elements.disabled" 
+             :class="elements.disabled ? disabled : active" class="is-pulled-right" 
+             @click="onclickJoin(elements.disabled)">{{ elements.linkText }}</a>
         </div>
       </div>
     </div>
