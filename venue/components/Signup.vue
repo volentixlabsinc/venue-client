24088f79552aa8746bf0a96d928490bb4d44c996
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-07-11 14:15:40 -0400

    Validate signup onblur

--- venue/components/Signup.vue
+++ venue/components/Signup.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <h3 class="title">SIGNUP</h3>
+    <h3 class="title">SIGN UP</h3>
     <form method="POST" @submit.prevent="registerUser">
-      <b-field label="Email">
-        <b-input v-model="email" type="email" name="email" required/>
+      <b-field :type="emailFieldType" :message="emailMessage" label="Email">
+        <b-input v-model="email" type="email" name="email" required @blur="checkEmail()"/>
       </b-field>
-      <b-field label="Username">
-        <b-input v-model="username" name="username" maxlength="25" required />
+      <b-field :type="usernameFieldType" :message="usernameMessage" label="Username">
+        <b-input v-model="username" name="username" maxlength="25" required @blur="checkUsername()" />
       </b-field>
       <b-field label="Password">
         <b-input v-model="password" minlength="6" type="password" name="password" required password-reveal/>
@@ -21,50 +21,72 @@
         <button class="button is-primary" type="submit">Sign Up</button>
       </b-field>
     </form>
-    <FeedbackModal v-if="ready" @feedbackEmits="recieveAction"/>
+    <b-modal :active.sync="isSuccessModalActive" @close="$router.push('/')">
+      <div class="modal-card">
+        <div class="modal-card-body">
+          Please click the email verification link we've just emailed to you to activate your Venue account.
+        </div>
+        <div class="modal-card-foot">
+          <button class="button" type="button" @click="isSuccessModalActive = false; $router.push('/')">Close</button>
+        </div>
+      </div>
+    </b-modal>
   </div>
 </template>
 
 <script>
-import FeedbackModal from "~/components/FeedbackModal.vue";
 export default {
-  components: {
-    FeedbackModal
-  },
   data() {
     return {
       ready: false,
       email: "",
+      emailFieldType: "",
+      emailMessage: "",
       username: "",
+      usernameFieldType: "",
+      usernameMessage: "",
       password: "",
-      signUpError: false,
-      newsletter: false
+      newsletter: false,
+      isSuccessModalActive: false
     };
   },
   mounted() {
     this.ready = true;
   },
   methods: {
-    // TODO Call this after username is entered
     async checkEmail() {
-      const params = {
-        email: this.email
-      };
-      const data = await this.$axios.$get("check/email-exists/", { params });
-      if (data.email_exists) {
-        this.$modal.show("FeedbackModal", {
-          type: "error",
-          title: "Error",
-          message: "This email account already exists",
-          buttonText: "CLOSE",
-          sendActionToFeedback: false
-        });
+      if (this.email.length > 0 && this.email.includes("@")) {
+        const params = { email: this.email };
+        const data = await this.$axios.$get("check/email-exists/", { params });
+        if (data.email_exists) {
+          this.emailFieldType = "is-danger";
+          this.emailMessage = "This email account already exists";
+        } else {
+          this.emailFieldType = "is-success";
+          this.emailMessage = "";
+        }
       } else {
-        this.registerUser();
+        this.emailFieldType = "is-danger";
+      }
+    },
+    async checkUsername() {
+      if (this.username.length > 0) {
+        const params = { username: this.username };
+        const data = await this.$axios.$get("check/username-exists/", {
+          params
+        });
+        if (data.username_exists) {
+          this.usernameFieldType = "is-danger";
+          this.usernameMessage = "This username has already been claimed";
+        } else {
+          this.usernameFieldType = "is-success";
+          this.usernameMessage = "";
+        }
+      } else {
+        this.usernameFieldType = "is-danger";
       }
     },
     async registerUser() {
-      this.signUpError = false;
       const result = await this.$axios.$post("create/user/", {
         email: this.email,
         username: this.username,
@@ -73,21 +95,9 @@ export default {
         receive_emails: this.newsletter
       });
 
-      if (result.status !== "success") {
-        this.signUpError = true;
-      } else {
-        this.$modal.show("FeedbackModal", {
-          type: "success",
-          title: "Success!",
-          message:
-            "Please click the email verification link we've just emailed to you to activate your Venue account.",
-          buttonText: "CLOSE",
-          sendActionToFeedback: true
-        });
+      if (result.success) {
+        this.isSuccessModalActive = true;
       }
-    },
-    recieveAction() {
-      this.$router.push("/");
     }
   }
 };
