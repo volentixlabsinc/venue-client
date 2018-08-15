8e1a9e74a0940f7fe45f1f416998a7932e7dce9c
Mojganahk <myrahazone2011@gmail.com>
2018-08-15 10:09:25 -0400

    turkish version of the Venue client(JS)

---
+++ venue/lang/tr-TR.js
@@ -0,0 +1,144 @@
+module.exports = {
+  nav: {
+    leaderboard: "Skor tablosu",
+    dashboard: "Kontrol paneli",
+    join_campaign: "Kampanyaya katýl",
+    my_profile: "Profilim",
+    sign_up: "Kaydol",
+    log_in: "Giriþ",
+    log_out: "Çýkýþ"
+  },
+  auth: {
+    forgot_password: "Þirenizi mi unuttunuz?",
+    title_reset_password: "Þifreyi sýfýrla",
+    title_new_password: "Yeni þifrenizi girin",
+    msg_reset_password:
+      "Þifrenizi mi unuttunuz? Sorun deðil. Eposta adresinizi girerseniz, þifrenizi sýfýrlamak için kullanabileceðiniz bir kod göndereceðiz.",
+    err_duplicate_email: "Bu eposta hesabý zaten var",
+    err_already_claimed: "Bu kullanýcý adý zaten kullanýmda",
+    msg_verified: "Eposta adresi doðrulandý",
+    err_password: "Hatalý kullanýcý adý ya da þifre.",
+    msg_not_verified:
+      "Eposta adresiniz doðrulanmadý. Lütfen eposta hesabýnýzý kontrol edin.",
+    msg_newsletter: "Beni ayrýca haber bülteni daðýtým listesine de ekle",
+    msg_policies:
+      "Kaydolarak, Volentix <a href='{0}' target='_blank'>gizlilik politikasý</a> ve <a href='{1}' target='_blank'>kullaným þartlarýný</a> kabul etmiþ sayýlýrsýnýz",
+    msg_email_verification:
+      "Venue hesabýnýzý aktive etmek için, lütfen size gönderdiðimiz epostadaki doðrulama baðlantýsýna týklayýn.",
+    msg_logging_out: "Çýkýþ yapýlýyor... "
+  },
+  bct_onboarding: {
+    err_duplicate_id: "Bu kimlik zaten bir Venue profiline baðlanmýþ",
+    err_min_forum_level:
+      "Bu kampanyaya katýlmak için, en az tam yetkili bir Bitcointalk üyesi olmalýsýnýz",
+    err_profile_not_found:
+      "Profil bulunamadý; lütfen doðru kullanýcý kimliðini kopyaladýðýnýzdan emin olun",
+    help_signature_1:
+      "1) Profil Sekmesinin içindeki Forum Profil Bilgilerine týklayýn",
+    help_signature_2:
+      "2) Ýmza kýsmýna gelene dek aþaðý kaydýrýn ve Metin Kutusuna kodu yapýþtýrýn",
+    help_signature_3: "3) Aþaðý doðru kaydýrýn ve deðiþiklikleri onaylayýn",
+    bct_welcome: "Bitcoin Talk kampanyasýna hoþ geldiniz",
+    help_find_bct_id: "Bitcointalk kullanýcý kimliðimi nasýl bulurum?",
+    msg_choose_signature: "Ýmzanýnýzý seçin",
+    bct_step_title: "Adým { step }",
+    bct_signature_paste:
+      "Þimdi de kopyaladýðýnýz kodu Bitcointalk profilinize yapýþtýrýn.",
+    msg_sig_update_how: "Bitcointalk imzamý nasýl güncellerim?",
+    btn_verify_now: "Þimdi doðrula",
+    paste_id: "Aþaðýdaki URL'den forum kimliðinizi yapýþtýrýn:"
+  },
+  settings: {
+    title_my_profile: "Profilim",
+    vtx_wallet: "VTX cüzdan adresi",
+    desc_vtx_wallet: "Ödemelerin gönderileceði VTX adresi",
+    btn_vtx_wallet: "VTX Adresini Belirle",
+    title_email: "Eposta adresi",
+    desc_email: "Kayýtlý eposta adresinizi deðiþtirin",
+    label_new_email: "Yeni eposta adresi",
+    label_change_email: "Eposta adresini deðiþtir",
+    btn_email: "Epostayý deðiþtir",
+    username: "Kullanýcý adý",
+    desc_username: "Kullanýcý adýnýzý deðiþtirin",
+    btn_username: "Kullanýcý adýný deðiþtir",
+    new_username: "Yeni kullanýcý adý",
+    password: "Þifre",
+    desc_password: "Þifrenizi deðiþtirin",
+    btn_password: "Þifreyi deðiþtir",
+    label_current_password: "Þu anki þifre",
+    label_new_password: "Yeni þifre",
+    language: "Dil",
+    btn_language: "Dili deðiþtir",
+    desc_language: "Site dilini deðiþtir",
+    two_factor: "Ýki-aþamalý doðrulama",
+    desc_two_factor: "Ýki aþamalý doðrulamayý deðiþtir",
+    btn_set_language: "Dili Belirle",
+    btn_enable_2fa: "Ýki aþamalý doðrulamayý etkinleþtir"
+  },
+  labels: {
+    my_rank: "Sýralamam",
+    my_posts: "Gönderilerim",
+    my_points: "Puanlarým",
+    participants: "Katýlýmcýlar",
+    total_posts: "Toplam gönderi",
+    current: "Þu anki",
+    available_rewards: "Kullanýlabilir ödüller",
+    my_current_rewards: "Þu anki ödüllerim",
+    my_campaign_activity: "Kampanya aktivitem",
+    posts: "0 gönderi | 1 gönderi | { count } gönderi",
+    points: "0 puan | 1 puan | { count } puan",
+    username: "Kullanýcý adý",
+    coming_soon: "Çok yakýnda",
+    num_posts: "Gönderi Sayýsý",
+    card_title: "Adým { imageNumber } - 3",
+    email: "Eposta"
+  },
+  points_details: {
+    title: "Puanlar / ödüller detaylý bilgi",
+    posts_sitewide:
+      "Site çapýnda 0 gönderi | Site çapýnda 1 gönderi | Site çapýnda { count } gönderi",
+    points_sitewide:
+      "Site çapýnda 0 puan | Site çapýnda 1 puan | Site çapýnda { count } puan",
+    your_total_posts: "Toplam gönderi sayýnýz",
+    your_total_points: "Toplam puanýnýz",
+    vtx_per_points: "Puan baþýna VTX"
+  },
+  titles: {
+    campaigns: "Kampanyalar",
+    bounties: "Centilmenlik Kampanyalarý",
+    proposals: "Teklifler",
+    leaderboard: "Skor tablosu",
+    bct_campaign: "Bitcointalk kampanyasý",
+    my_campaign_activity: "Kampanya aktivitelerim",
+    bct_leaderboard: "Bitcointalk skor tablosu",
+    earn_vtx: "VTX Kazan!",
+    my_rewards: "Ödüllerim",
+    login: "Giriþ"
+  },
+  edit_signature: {
+    current_signature: "Þu anki imza",
+    available_signatures: "Kullanýlabilir imzalar",
+    verify_title: "Ýmza kopyalandý"
+  },
+  messages: {
+    join_bitcointalk: "Bitcointalk imza kampanyasýna katýlýn",
+    signature_warning_1:
+      "Ýmzalarýn ilk 24 saat içinde en az 23 saat boyunca kullanýlmasý gerektiðini unutmayýn. Bu süre sona ermeden imzayý deðiþtirirseniz, o gönderi için tüm puanlarý kaybedersiniz.",
+    signature_warning_2: "Bu iþlemin puan bakiyenize yansýmasý",
+    signature_warning_3: " 24 saate kadar sürebilir.",
+    steps: "Step {0} of {1}"
+  },
+  buttons: {
+    btn_view_details: "Detaylarý görüntüle",
+    btn_edit_signature: "Ýmzayý düzenle",
+    btn_cancel: "Ýptal",
+    btn_report_bug: "Hata bildir",
+    btn_join_now: "Þimdi katýl",
+    btn_previous: "Önceki",
+    btn_next: "Ýleri",
+    btn_close: "Kapat",
+    btn_copy_code: "Kodu kopyala",
+    btn_change: "Deðiþtir",
+    btn_verify: "Kopyala"
+  }
+};
