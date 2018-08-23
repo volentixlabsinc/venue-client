d0a183e89ebf77bf38fd092ed5d3b49fba73bee3
Ksenia Popova <p.ksyusha@gmail.com>
2018-08-23 14:58:39 -0400

    Host the Turkish email version on Venue

--- venue/pages/tr/ReferralEmail.vue
+++ venue/pages/tr/ReferralEmail.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="column col-md-6">
+  <div class="columns is-centered">
+    <div class="column is-half">
+      <div class="row p-t-xl p-b-lg">
         <div class="logo has-text-centered">
-          <img src="~assets/logo.png" class="img-responsive">
+          <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/logo.png">
         </div>
         <div class="content">
-          <p>Merhaba,</p>
-          <p>İletişim bilgileriniz, Volentix Projesi ile ilgilenebileceğinizi düşünen {{ username }} tarafından sağlandı.</p>
-          <p>Volentix projesine dahil olmanın yollarından biri de,
+          <p class="no-padding">Merhaba,</p>
+          <p class="no-padding">İletişim bilgileriniz, Volentix Projesi ile ilgilenebileceğinizi düşünen {{ username }} tarafından sağlandı.</p>
+          <p class="no-padding">Volentix projesine dahil olmanın yollarından biri de,
           <a target="_blank" href="https://bitcointalk.org/">bitcointalk.org</a> imza kampanyasına katılmaktır.</p>
-          <p>Eğer Bitcointalk hesabınız şu seviyelerden biriyse, siz de kampanyaya katılabilirsiniz:</p>
+          <p class="no-padding">Eğer Bitcointalk hesabınız şu seviyelerden biriyse, siz de kampanyaya katılabilirsiniz:</p>
 
-          <ul>
+          <ul class="p-t-md p-b-md">
             <li>Üye (Member)</li>
             <li>Tam Üye (Full Member)</li>
             <li>Eski Üye (Sr. Member)</li>
@@ -24,21 +24,22 @@
           takip işlemini otomatikleştirmiştir ve güncel sonuçları bir skor tablosu üzerinde yayımlar. Volentix imzasını
           mesajlarınıza eklemek kolaydır ve kazandığınız VTX'leri anlık olarak görebildiğiniz için hem şeffaf, hem de eğlenceli
           bir sistemdir. </p>
-          <p class="top-padding">Bu imza kampanyasına katılırsanız, {{ username }} bir referans ödülü kazanacaktır. Ayrıca siz de Volentix ödül kampanyalarına
+
+          <p>Bu imza kampanyasına katılırsanız, {{ username }} bir referans ödülü kazanacaktır. Ayrıca siz de Volentix ödül kampanyalarına
           katılarak VTX kazanma şansı elde edeceksiniz. Ödüller, bir Volentix imzası barındıran mesajların sayısına ve
           imzalı mesajların gönderim tarihine göre belirlenecektir.</p>
-          <p class="text-center top-padding bold">Lütfen kayıt olmak için buraya tıklayın.</p>
-
+          <p class="has-text-centered bold is-size-5">
+            <a :href="toSignUp" class="has-text-centered bold">Lütfen kayıt olmak için buraya tıklayın.</a>
+          </p>
           <a target="_blank" href="https://www.youtube.com/watch?v=CUSy7bVnnVg">
             <p class="player-button background has-text-centered">
-              <img src="~assets/play-button.jpg" class="img-responsive">
+              <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/play-button.jpg">
             </p>
           </a>
 
-          <p class="text-center padding-top-10px">Eğer siz de arkadaşlarınızı Venue web uygulamasındaki Bitcointalk imza kampanyasına katılmaya</p>
-          <p class="text-center">davet etmek istiyorsanız, lütfen
-          <a target="_blank" href="https://venue.volentix.io/">venue.volentix.io</a> adresini ziyaret edin ve kaydolun. Böylelikle siz</p>
-          <p class="text-center padding-bottom-10px">de kendi referans kodunuzu diğerlerine gönderebilirsiniz. </p>
+          <div class="clearfix p-b-md"/>
+
+          <p class="has-text-centered">Eğer siz de arkadaşlarınızı Venue web uygulamasındaki Bitcointalk imza kampanyasına katılmaya davet etmek istiyorsanız, lütfen <a target="_blank" href="https://venue.volentix.io/">venue.volentix.io</a> adresini ziyaret edin ve kaydolun. Böylelikle siz de kendi referans kodunuzu diğerlerine gönderebilirsiniz. </p>
 
           <div class="background has-text-centered color-white">
             <p class="p-t-md"> Volentix, VTX tarafından desteklenen ve bir kripto para birimine dayalı merkezi olmayan bir </p>
@@ -46,30 +47,30 @@
             <a target="_blank" class="color-white" href="https://volentix.io/">Volentix</a> ekosistemi hakkında kısa bir tanıtımı aşağıda bulabilirsiniz:</p>
           </div>
 
-          <div class="clearfix padding-bottom-10px"/>
+          <div class="clearfix p-b-md"/>
 
           <div class="level">
-            <div class="level-item bottom-block small-logo padding-bottom-10px">
+            <div class="level-item bottom-block p-b-sm">
               <div>
-                <p class="has-text-centered">
+                <p class="has-text-centered p">
                   <a target="_blank" href="https://venue.volentix.io/">
-                    <img src="~assets/img1.png" class="img-responsive">
+                    <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/img1.png">
                   </a>
                 </p>
-                <p>Topluluk oluşturma faaliyetleri veya Volentix ekosistemine yapılıp topluluğa fayda sağlayan katkılar karşılığında
+                <p class="p-r-sm p-l-sm">Topluluk oluşturma faaliyetleri veya Volentix ekosistemine yapılıp topluluğa fayda sağlayan katkılar karşılığında
                 kullanıcıların VTX jetonlarını kazanmalarını sağlayan dinamik bir topluluk platformudur. Yol haritası: Temmuz
                 2018'de yayımlandı.
                 </p>
               </div>
             </div>
-            <div class="level-item bottom-block small-logo padding-bottom-10px">
+            <div class="level-item bottom-block p-b-sm">
               <div>
                 <p class="has-text-centered">
                   <a target="_blank" href="https://s3.ca-central-1.amazonaws.com/volentix/VDexWhitePaper-v0.1.0.pdf">
-                    <img src="~assets/img2.png" class="img-responsive">
+                    <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/img2.png">
                   </a>
                 </p>
-                <p>EOS üzerine kurulu merkezi olmayan bir kripto para birimi takas aracıdır. Kripto kullanıcılarının farklı blokzincirleri
+                <p class="p-r-sm p-l-sm">EOS üzerine kurulu merkezi olmayan bir kripto para birimi takas aracıdır. Kripto kullanıcılarının farklı blokzincirleri
                 arasında eşler arası finansal işlemler gerçekleştirmelerini sağlar. Kullanıcı bilgileri ile menkul kıymetleri
                 merkezi bir yerde depolamaz ve kaydetmez.
                   <a target="_blank" href="https://s3.ca-central-1.amazonaws.com/volentix/VDexWhitePaper-v0.1.0.pdf">
@@ -79,30 +80,30 @@
             </div>
           </div>
 
-          <div class="clearfix padding-bottom-10px"/>
+          <div class="clearfix p-b-sm"/>
 
           <div class="level">
-            <div class="level-item bottom-block small-logo padding-bottom-10px">
+            <div class="level-item bottom-block p-b-sm">
               <div>
                 <p class="has-text-centered">
                   <a target="_blank" href="https://venue.volentix.io/">
-                    <img src="~assets/img3.png" class="img-responsive">
+                    <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/img3.png">
                   </a>
                 </p>
-                <p>TVDEX ve Volentix Ekosistemindeki diğer DApp'lar üzerinde kullanılabilen yerel ve çok sayıda para birimini destekleyen
+                <p class="p-r-sm p-l-sm">TVDEX ve Volentix Ekosistemindeki diğer DApp'lar üzerinde kullanılabilen yerel ve çok sayıda para birimini destekleyen
                 cüzdan. Yol haritası: Lütfen
                   <a target="_blank" href="https://s3.ca-central-1.amazonaws.com/volentix/VDexWhitePaper-v0.1.0.pdf">VDEX tanıtım belgesine</a> göz atın.
                 </p>
               </div>
             </div>
-            <div class="level-item bottom-block small-logo padding-bottom-10px">
+            <div class="level-item bottom-block p-b-sm">
               <div>
                 <p class="has-text-centered">
                   <a target="_blank" href="https://s3.ca-central-1.amazonaws.com/volentix/VDexWhitePaper-v0.1.0.pdf">
-                    <img src="~assets/img4.png" class="img-responsive">
+                    <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/img4.png">
                   </a>
                 </p>
-                <p>Kripto para birimlerini analiz etmek için makine öğrenim algoritmalarını kullanan analitik bir motor olup, verilerini
+                <p class="p-r-sm p-l-sm">Kripto para birimlerini analiz etmek için makine öğrenim algoritmalarını kullanan analitik bir motor olup, verilerini
                 şu kaynaklardan alır: Piyasa Verileri, Sosyal duyarlılık analizi, Teknik analizler. Yol haritası: Tanıtım belgesi
                 2018 yazı.
                 </p>
@@ -110,46 +111,46 @@
             </div>
           </div>
 
-          <div class="clearfix padding-bottom-10px"/>
+          <div class="clearfix p-b-sm"/>
 
-          <p class="bold padding-top-10px padding-bottom-10px text-center font-size-16px has-text-centered">Bizi Sosyal Medya Üzerinde Takip Edin</p>
+          <p class="bold p-t-sm p-b-sm text-center is-size-6 has-text-centered">Bizi Sosyal Medya Üzerinde Takip Edin</p>
 
-          <div class="icons text-center padding-top-10px padding-bottom-10px">
+          <div class="icons text-center p-t-sm p-b-sm">
             <div class="level">
               <div class="level-item">
                 <a target="_blank" href="https://twitter.com/Volentix">
-                  <img src="~assets/icon1.png" class="img-responsive">
+                  <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/icon1.png">
                 </a>
               </div>
               <div class="level-item">
                 <a target="_blank" href="https://t.me/volentix">
-                  <img src="~assets/icon2.png" class="img-responsive">
+                  <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/icon2.png">
                 </a>
               </div>
               <div class="level-item">
                 <a target="_blank" href="https://github.com/Volentix">
-                  <img src="~assets/icon3.png" class="img-responsive">
+                  <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/icon3.png">
                 </a>
               </div>
               <div class="level-item">
                 <a target="_blank" href="https://www.linkedin.com/company/volentix-labs/">
-                  <img src="~assets/icon4.png" class="img-responsive">
+                  <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/icon4.png">
                 </a>
               </div>
               <div class="level-item">
                 <a target="_blank" href="https://www.youtube.com/channel/UCGjUbCE-kht1gK6y5neD1cQ">
-                  <img src="~assets/icon5.png" class="img-responsive">
+                  <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/icon5.png">
                 </a>
               </div>
             </div>
           </div>
 
-          <div class="clearfix padding-bottom-10px"/>
+          <div class="clearfix p-b-sm"/>
 
           <div class="level">
-            <div class="level-item bottom-block has-text-centered">
+            <div class="level-item bottom-block has-text-centered logo-bottom">
               <a target="_blank" href="https://volentix.io/">
-                <img src="~assets/img5.png">
+                <img src="http://dm7lsapjc0ehv.cloudfront.net/referral-email/img5.png">
               </a>
             </div>
             <div class="level-item bottom-block has-text-centered">
@@ -159,12 +160,12 @@
         </div>
       </div>
     </div>
-    <a :href="toSignUp">Follow this link to sign up</a>
   </div>
 </template>
 
 <script>
 export default {
+  layout: "email",
   data() {
     let data = {
       toSignUp: "",
@@ -177,26 +178,24 @@ export default {
       location.protocol +
       "//" +
       location.host +
-      "/signup?referrer_name=" +
-      this.$route.query.referrer_name +
-      "&code=" +
+      "/signup?code=" +
       this.$route.query.code;
     this.username = this.$route.query.referrer_name;
-  },
-  methods: {}
+  }
 };
 </script>
 
 <style scoped>
 .row {
-  color: #4f0357;
-  font-size: 14px;
+  color: black;
+  font-size: 0.9rem;
   font-family: Calibri, Helvetica, Arial, sans-serif;
-  padding-top: 60px;
-  padding-bottom: 40px;
 }
-p {
-  margin-bottom: 0;
+.no-padding {
+  margin-bottom: 0 !important;
+}
+.row li {
+  margin: 0;
 }
 a,
 a:hover,
@@ -207,21 +206,12 @@ a:visited {
 }
 .logo img {
   margin: 0 auto;
-  width: 250px;
+  width: 16rem;
 }
 .content {
-  padding: 20px;
-}
-.content ul {
-  padding-top: 30px;
-  padding-bottom: 30px;
-  margin-bottom: 0;
-}
-.content p.top-padding {
-  padding-top: 25px;
+  padding: 2rem;
 }
 .content p.bold {
-  font-size: 18px;
   font-family: Calibri, Helvetica, Arial, sans-serif;
   font-weight: bold;
   color: rgb(79, 3, 87);
@@ -230,41 +220,19 @@ a:visited {
   background-color: rgb(61, 2, 67);
 }
 .content .player-button {
-  margin-top: 10px;
+  margin-top: 1rem;
 }
 .content .player-button img {
-  width: 250px;
+  width: 15rem;
   margin: 0 auto;
-  padding: 15px 0;
-}
-.content .padding-top-10px {
-  padding-top: 10px;
-}
-.content .padding-bottom-10px {
-  padding-bottom: 10px;
+  padding: 1rem 0;
 }
 .content .color-white {
   color: white;
 }
-
-.content .small-logo img {
-  width: 150px;
-  margin: 0 auto;
-  height: 50px;
-}
-
-.content .font-size-16px {
-  font-size: 16px !important;
-}
-
 .content .icons img {
-  width: 30px;
+  width: 2rem;
   margin: 0 auto;
-}
-
-.content .footer img {
-  width: 200px;
-  padding-left: 25px;
 }
 .content .custom-btn {
   background-color: rgb(61, 2, 67);
@@ -273,17 +241,15 @@ a:visited {
   border: none;
   border-radius: 0;
   outline: none;
-  margin-top: 15px;
-  margin-left: 25px;
+  margin-top: 1rem;
+  margin-left: 2rem;
   font-weight: bold;
 }
-
 .bottom-block {
-  width: 50% !important;
+  width: 40% !important;
   margin-bottom: auto;
 }
-
 .bottom-block img {
-  width: 60%;
+  height: 2rem;
 }
 </style>
