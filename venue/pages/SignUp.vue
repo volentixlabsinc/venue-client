d983ab33f11e351d59d444d764e3300cd6f5c425
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-09-12 13:27:17 -0400

    Change new signup message box to bluegrey

--- venue/pages/SignUp.vue
+++ venue/pages/SignUp.vue
@@ -4,8 +4,13 @@
       <Signup class="p-l-md"/>
     </div>
     <div slot="right">
-      <article class="message is-warning m-r-lg">
-        <div class="message-header">{{ $t('auth.no_us_header') }}</div>
+      <article class="message m-r-lg">
+        <div class="message-header">
+          <div>
+            <i class="fas fa-ban p-r-sm"/>
+            <span>{{ $t('auth.no_us_header') }}</span>
+          </div>
+        </div>
         <div class="message-body">
           <div>{{ $t('auth.no_us_1') }}</div>
           <div class="p-t-md">{{ $t('auth.no_us_2') }}</div>
