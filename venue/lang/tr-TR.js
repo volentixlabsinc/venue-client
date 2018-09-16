bb6ff00e913f0a8dd539b29bd4ede4c9d2165da2
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-09-16 09:48:16 -0400

    Update with correct translation text

--- venue/lang/tr-TR.js
+++ venue/lang/tr-TR.js
@@ -1,180 +1,180 @@
-module.exports = {
+﻿module.exports = {
   nav: {
     leaderboard: "Skor tablosu",
     dashboard: "Kontrol paneli",
-    join_campaign: "Kampanyaya katýl",
+    join_campaign: "Kampanyaya katıl",
     my_profile: "Profilim",
     sign_up: "Kaydol",
-    log_in: "Giriþ",
-    log_out: "Çýkýþ",
-    invite_friend: "Arkadaþýný davet et"
+    log_in: "Giriş",
+    log_out: "Çıkış",
+    invite_friend: "Arkadaşını davet et"
   },
   auth: {
-    forgot_password: "Þirenizi mi unuttunuz?",
-    title_reset_password: "Þifreyi sýfýrla",
-    title_new_password: "Yeni þifrenizi girin",
+    forgot_password: "Şifrenizi mi unuttunuz?",
+    title_reset_password: "Şifreyi sıfırla",
+    title_new_password: "Yeni şifrenizi girin",
     msg_reset_password:
-      "Þifrenizi mi unuttunuz? Sorun deðil. Eposta adresinizi girerseniz, þifrenizi sýfýrlamak için kullanabileceðiniz bir kod göndereceðiz.",
-    err_duplicate_email: "Bu eposta hesabý zaten var",
-    err_already_claimed: "Bu kullanýcý adý zaten kullanýmda",
-    msg_verified: "Eposta adresi doðrulandý",
-    err_password: "Hatalý kullanýcý adý ya da þifre.",
+      "Şifrenizi mi unuttunuz? Sorun değil. Eposta adresinizi girerseniz, şifrenizi sıfırlamak için kullanabileceğiniz bir kod göndereceğiz.",
+    err_duplicate_email: "Bu eposta hesabı zaten var",
+    err_already_claimed: "Bu kullanıcı adı zaten kullanımda",
+    msg_verified: "Eposta adresi doğrulandı",
+    err_password: "Hatalı kullanıcı adı ya da şifre.",
     msg_not_verified:
-      "Eposta adresiniz doðrulanmadý. Lütfen eposta hesabýnýzý kontrol edin.",
-    msg_newsletter: "Beni ayrýca haber bülteni daðýtým listesine de ekle",
+      "Eposta adresiniz doğrulanmadı. Lütfen eposta hesabınızı kontrol edin.",
+    msg_newsletter: "Beni ayrıca haber bülteni dağıtım listesine de ekle",
     msg_policies:
-      "Kaydolarak, Volentix <a href='{0}' target='_blank'>gizlilik politikası</a> ve <a href='{1}' target='_blank'>kullanım şartlarını</a> kabul etmiş sayılırsınız. Ayrıca, Volentix haber bültenine eklenmeyi de kabul etmiş sayılırsınız.",
+      "Kaydolarak, Volentix <a href='{0}' target='_blank'>gizlilik politikası</a> ve <a href='{1}' target='_blank'>kullanım şartlarını</a> kabul etmiş sayılırsınız",
     msg_email:
-      "Ayrýca, Volentix haber bültenine eklenmeyi de kabul etmiþ sayýlýrsýnýz.",
+      "Ayrıca, Volentix haber bültenine eklenmeyi de kabul etmiş sayılırsınız.",
     msg_email_verification:
-      "Venue hesabýnýzý aktive etmek için, lütfen size gönderdiðimiz epostadaki doðrulama baðlantýsýna týklayýn.",
+      "Venue hesabınızı aktive etmek için, lütfen size gönderdiğimiz epostadaki doğrulama bağlantısına tıklayın.",
     msg_email_check_spam_folder:
-      "Nadiren, doðrulama epostasý spam olarak iþaretlenebilir. Eðer bizden bir eposta almadýysanýz, lütfen spam klasörünüzü kontrol etmeyi unutmayýn. Teþekkürler.",
-    msg_logging_out: "Çýkýþ yapýlýyor... ",
-    no_us_header: "ABD VATANDAÞLARI KABUL EDÝLMEZ",
+      "Nadiren, doğrulama epostası spam olarak işaretlenebilir. Eğer bizden bir eposta almadıysanız, lütfen spam klasörünüzü kontrol etmeyi unutmayın. Teşekkürler.",
+    msg_logging_out: "Çıkış yapılıyor... ",
+    no_us_header: "ABD VATANDAŞLARI KABUL EDİLMEZ",
     no_us_1:
-      "VTX, ABD vatandaþlarýna sunulmamaktadýr. Eðer eyaletleri ve Columbia dahil olmak üzere bölgeleri içinde yaþýyorsanýz veya ABD vatandaþýysanýz, VENUE'yi kullanmayýn, VERTO'yu yüklemeyin ve kullanmayýn, VTX satýn almayýn. ABD vatandaþý veya ABD içinde yaþayan tanýmýna hiçbir sýnýrlama olmaksýzýn gerçek ve tüzel kiþilikler, dernekler ve diðer her türlü resmi/gayriresmi kuruluþ dahildir.",
+      "VTX, ABD vatandaşlarına sunulmamaktadır. Eğer eyaletleri ve Columbia dahil olmak üzere bölgeleri içinde yaşıyorsanız veya ABD vatandaşıysanız, VENUE'yi kullanmayın, VERTO'yu yüklemeyin ve kullanmayın, VTX satın almayın. ABD vatandaşı veya ABD içinde yaşayan tanımına hiçbir sınırlama olmaksızın gerçek ve tüzel kişilikler, dernekler ve diğer her türlü resmi/gayriresmi kuruluş dahildir.",
     no_us_2:
-      "VENUE veya VERTO'yu kullanarak, bu hükmü ve diðer kurallar ile þartlarý kabul etmiþ sayýlýrsýnýz."
+      "VENUE veya VERTO'yu kullanarak, bu hükmü ve diğer kurallar ile şartları kabul etmiş sayılırsınız."
   },
   bct_onboarding: {
-    err_duplicate_id: "Bu kimlik zaten bir Venue profiline baðlanmýþ",
+    err_duplicate_id: "Bu kimlik zaten bir Venue profiline bağlanmış",
     err_min_forum_level:
-      "Bu kampanyaya katýlmak için, en az tam yetkili bir Bitcointalk üyesi olmalýsýnýz",
-    err_profile_not_found: "Profil bulunamadý.",
+      "Bu kampanyaya katılmak için, en az tam yetkili bir Bitcointalk üyesi olmalısınız",
+    err_profile_not_found: "Profil bulunamadı.",
     err_profile_check_user_id:
-      "Lütfen doðru kullanýcý kimliðini kopyaladýðýnýzý doðrulayýn",
-    err_error_is_received: "HTTP {code} alýndý",
+      "Lütfen doğru kullanıcı kimliğini kopyaladığınızı doğrulayın",
+    err_error_is_received: "HTTP {code} alındı",
     help_signature_1:
-      "1) Profil Sekmesinin içindeki Forum Profil Bilgilerine týklayýn",
+      "1) Profil Sekmesinin içindeki Forum Profil Bilgilerine tıklayın",
     help_signature_2:
-      "2) Ýmza kýsmýna gelene dek aþaðý kaydýrýn ve Metin Kutusuna kodu yapýþtýrýn",
-    help_signature_3: "3) Aþaðý doðru kaydýrýn ve deðiþiklikleri onaylayýn",
-    bct_welcome: "Bitcoin Talk kampanyasýna hoþ geldiniz",
-    help_find_bct_id: "Bitcointalk kullanýcý kimliðimi nasýl bulurum?",
-    msg_choose_signature: "Ýmzanýnýzý seçin",
-    bct_step_title: "Adým { step }",
+      "2) İmza kısmına gelene dek aşağı kaydırın ve Metin Kutusuna kodu yapıştırın",
+    help_signature_3: "3) Aşağı doğru kaydırın ve değişiklikleri onaylayın",
+    bct_welcome: "Bitcoin Talk kampanyasına hoş geldiniz",
+    help_find_bct_id: "Bitcointalk kullanıcı kimliğimi nasıl bulurum?",
+    msg_choose_signature: "İmzanınızı seçin",
+    bct_step_title: "Adım { step }",
     bct_signature_paste:
-      "Þimdi de kopyaladýðýnýz kodu Bitcointalk profilinize yapýþtýrýn.",
-    msg_sig_update_how: "Bitcointalk imzamý nasýl güncellerim?",
-    btn_verify_now: "Þimdi doðrula",
-    paste_id: "Aþaðýdaki URL'den forum kimliðinizi yapýþtýrýn:",
-    your_user_id: "Kullanýcý kimliðiniz"
+      "Şimdi de kopyaladığınız kodu Bitcointalk profilinize yapıştırın.",
+    msg_sig_update_how: "Bitcointalk imzamı nasıl güncellerim?",
+    btn_verify_now: "Şimdi doğrula",
+    paste_id: "Aşağıdaki URL'den forum kimliğinizi yapıştırın:",
+    your_user_id: "Kullanıcı kimliğiniz"
   },
   settings: {
     title_my_profile: "Profilim",
     vtx_wallet: "VTX cüzdan adresi",
-    desc_vtx_wallet: "Ödemelerin gönderileceði VTX adresi",
+    desc_vtx_wallet: "Ödemelerin gönderileceği VTX adresi",
     btn_vtx_wallet: "VTX Adresini Belirle",
     title_email: "Eposta adresi",
-    desc_email: "Kayýtlý eposta adresinizi deðiþtirin",
+    desc_email: "Kayıtlı eposta adresinizi değiştirin",
     label_new_email: "Yeni eposta adresi",
-    label_change_email: "Eposta adresini deðiþtir",
-    btn_email: "Epostayý deðiþtir",
-    username: "Kullanýcý adý",
-    desc_username: "Kullanýcý adýnýzý deðiþtirin",
-    btn_username: "Kullanýcý adýný deðiþtir",
-    new_username: "Yeni kullanýcý adý",
-    password: "Þifre",
-    desc_password: "Þifrenizi deðiþtirin",
-    btn_password: "Þifreyi deðiþtir",
-    label_current_password: "Þu anki þifre",
-    label_new_password: "Yeni þifre",
+    label_change_email: "Eposta adresini değiştir",
+    btn_email: "Epostayı değiştir",
+    username: "Kullanıcı adı",
+    desc_username: "Kullanıcı adınızı değiştirin",
+    btn_username: "Kullanıcı adını değiştir",
+    new_username: "Yeni kullanıcı adı",
+    password: "Şifre",
+    desc_password: "Şifrenizi değiştirin",
+    btn_password: "Şifreyi değiştir",
+    label_current_password: "Şu anki şifre",
+    label_new_password: "Yeni şifre",
     language: "Dil",
-    btn_language: "Dili deðiþtir",
-    desc_language: "Site dilini deðiþtir",
-    two_factor: "Ýki-aþamalý doðrulama",
-    desc_two_factor: "Ýki aþamalý doðrulamayý deðiþtir",
+    btn_language: "Dili değiştir",
+    desc_language: "Site dilini değiştir",
+    two_factor: "İki-aşamalı doğrulama",
+    desc_two_factor: "İki aşamalı doğrulamayı değiştir",
     btn_set_language: "Dili Belirle",
-    btn_enable_2fa: "Ýki aþamalý doðrulamayý etkinleþtir",
-    emails_input: "Arkadaþýnýzýn eposta adresi"
+    btn_enable_2fa: "İki aşamalı doğrulamayı etkinleştir",
+    emails_input: "Arkadaşınızın eposta adresi"
   },
   labels: {
-    my_rank: "Sýralamam",
+    my_rank: "Sıralamam",
     my_posts: "Gönderilerim",
-    my_points: "Puanlarým",
-    participants: "Katýlýmcýlar",
+    my_points: "Puanlarım",
+    participants: "Katılımcılar",
     total_posts: "Toplam gönderi",
-    current: "Þu anki",
-    available_rewards: "Kullanýlabilir ödüller",
-    my_current_rewards: "Þu anki ödüllerim",
+    current: "Şu anki",
+    available_rewards: "Kullanılabilir ödüller",
+    my_current_rewards: "Şu anki ödüllerim",
     my_campaign_activity: "Kampanya aktivitem",
     posts: "0 gönderi | 1 gönderi | { count } gönderi",
     points: "0 puan | 1 puan | { count } puan",
-    username: "Kullanýcý adý",
-    coming_soon: "Çok yakýnda",
-    num_posts: "Gönderi Sayýsý",
-    card_title: "Adým { imageNumber } - 3",
+    username: "Kullanıcı adı",
+    coming_soon: "Çok yakında",
+    num_posts: "Gönderi Sayısı",
+    card_title: "Adım { imageNumber } - 3",
     email: "Eposta",
     bonus_percent: "{rank} bonusu: {bonus}% (dahil)",
     earned_vtx: "{count} VTX",
-    invite_form_header: "Arkadaþýný davet et",
-    status_verified: "Bitcointalk Hesabý Doðrulandý",
+    invite_form_header: "Arkadaşını davet et",
+    status_verified: "Bitcointalk Hesabı Doğrulandı",
     status_pending: "Beklemede",
-    referrals_friends: "Arkadaþlar",
+    referrals_friends: "Arkadaşlar",
     referrals_status: "Durum",
     registered_friends:
-      "0 Kayýtlý arkadaþ | 1 Kayýtlý arkadaþ | Kayýtlý arkadaþ sayýsý: {count}"
+      "0 Kayıtlı arkadaş | 1 Kayıtlı arkadaş | Kayıtlı arkadaş sayısı: {count}"
   },
   points_details: {
-    title: "Puanlar / ödüller detaylý bilgi",
+    title: "Puanlar / ödüller detaylı bilgi",
     posts_sitewide:
-      "Site çapýnda 0 gönderi | Site çapýnda 1 gönderi | Site çapýnda { count } gönderi",
+      "Site çapında 0 gönderi | Site çapında 1 gönderi | Site çapında { count } gönderi",
     points: "{count} puan",
     points_sitewide:
-      "Site çapýnda 0 puan | Site çapýnda 1 puan | Site çapýnda { count } puan",
+      "Site çapında 0 puan | Site çapında 1 puan | Site çapında { count } puan",
     available_reward: " {count} adet ödül alabilirsiniz",
-    your_total_posts: "Toplam gönderi sayýnýz",
-    your_total_points: "Toplam puanýnýz",
-    vtx_per_points: "Puan baþýna VTX"
+    your_total_posts: "Toplam gönderi sayınız",
+    your_total_points: "Toplam puanınız",
+    vtx_per_points: "Puan başına VTX"
   },
   titles: {
     campaigns: "Kampanyalar",
-    bounties: "Centilmenlik Kampanyalarý",
+    bounties: "Centilmenlik Kampanyaları",
     proposals: "Teklifler",
     leaderboard: "Skor tablosu",
-    bct_campaign: "Bitcointalk kampanyasý",
+    bct_campaign: "Bitcointalk kampanyası",
     my_campaign_activity: "Kampanya aktivitelerim",
     bct_leaderboard: "Bitcointalk skor tablosu",
     earn_vtx: "VTX Kazan!",
     my_rewards: "Ödüllerim",
-    login: "Giriþ",
-    invite_a_friend: "Arkadaþlarýnýzý davet edin ve VTX kazanýn",
+    login: "Giriş",
+    invite_a_friend: "Arkadaşlarınızı davet edin ve VTX kazanın",
     invite_a_friend_subt:
-      "Bitcointalk kampanyasýna katýlarak bir Volentix imzasý belirleyen her kiþi baþýna {count} VTX kazanýn."
+      "Bitcointalk kampanyasına katılarak bir Volentix imzası belirleyen her kişi başına {count} VTX kazanın."
   },
   edit_signature: {
-    current_signature: "Þu anki imza",
-    available_signatures: "Kullanýlabilir imzalar",
-    verify_title: "Ýmza kopyalandý"
+    current_signature: "Şu anki imza",
+    available_signatures: "Kullanılabilir imzalar",
+    verify_title: "İmza kopyalandı"
   },
   messages: {
-    join_bitcointalk: "Bitcointalk imza kampanyasýna katýlýn",
+    join_bitcointalk: "Bitcointalk imza kampanyasına katılın",
     signature_warning_1:
-      "Ýmzalarýn ilk 24 saat içinde en az 23 saat boyunca kullanýlmasý gerektiðini unutmayýn. Bu süre sona ermeden imzayý deðiþtirirseniz, o gönderi için tüm puanlarý kaybedersiniz.",
-    steps: "Adým {0} kalan {1}",
+      "İmzaların ilk 24 saat içinde en az 23 saat boyunca kullanılması gerektiğini unutmayın. Bu süre sona ermeden imzayı değiştirirseniz, o gönderi için tüm puanları kaybedersiniz.",
+    steps: "Adım {0} kalan {1}",
     invite_friend_note:
-      "Birden çok kiþiyi ayýrt etmek için virgül ' , ' kullanýn",
+      "Birden çok kişiyi ayırt etmek için virgül ' , ' kullanın",
     email_is_sent: "Davetiniz gönderildi",
     incorrect_email_input: "Geçersiz eposta",
     social_sharing_text:
-      "Bu referans baðlantýsýný kullanýp venue.volentix.io'ya kayýt olun ve VTX ödülleri kazanýn. Volentix projesi bir topluluk giriþimi olup, kripto para birimlerinin deðerini ve güvenliðini artýran yazýlýmlar geliþtirir. VDEX'i duymuþ muydunuz? EOS üzerine inþa edilmiþ merkezi olmayan bir takas borsasýdýr. Belki artýk sizin de aramýza katýlma zamanýnýz gelmiþtir. ",
+      "Bu referans bağlantısını kullanıp venue.volentix.io'ya kayıt olun ve VTX ödülleri kazanın. Volentix projesi bir topluluk girişimi olup, kripto para birimlerinin değerini ve güvenliğini artıran yazılımlar geliştirir. VDEX'i duymuş muydunuz? EOS üzerine inşa edilmiş merkezi olmayan bir takas borsasıdır. Belki artık sizin de aramıza katılma zamanınız gelmiştir. ",
     social_sharing_twitter:
-      "Bu referans baðlantýsýný kullanýp venue.volentix.io'ya kayýt olun ve VTX ödülleri kazanýn. Volentix, kripto para birimlerinin deðerini ve güvenliðini artýran yazýlýmlar geliþtirir. VDEX'i duymuþ muydunuz? EOS üzerine inþa edilmiþ merkezi olmayan bir takas borsasýdýr. Belki artýk sizin de aramýza katýlma zamanýnýz gelmiþtir. "
+      "Bu referans bağlantısını kullanıp venue.volentix.io'ya kayıt olun ve VTX ödülleri kazanın. Volentix, kripto para birimlerinin değerini ve güvenliğini artıran yazılımlar geliştirir. VDEX'i duymuş muydunuz? EOS üzerine inşa edilmiş merkezi olmayan bir takas borsasıdır. Belki artık sizin de aramıza katılma zamanınız gelmiştir. "
   },
   buttons: {
-    btn_view_details: "Detaylarý görüntüle",
-    btn_edit_signature: "Ýmzayý düzenle",
-    btn_cancel: "Ýptal",
+    btn_view_details: "Detayları görüntüle",
+    btn_edit_signature: "İmzayı düzenle",
+    btn_cancel: "İptal",
     btn_report_bug: "Hata bildir",
-    btn_join_now: "Þimdi katýl",
+    btn_join_now: "Şimdi katıl",
     btn_previous: "Önceki",
-    btn_next: "Ýleri",
+    btn_next: "İleri",
     btn_close: "Kapat",
     btn_copy_code: "Kodu kopyala",
-    btn_change: "Deðiþtir",
+    btn_change: "Değiştir",
     btn_verify: "Kopyala",
     btn_send_email: "Gönder",
-    btn_copy_link: "Baðlantýyý kopyala"
+    btn_copy_link: "Bağlantıyı kopyala"
   }
 };
