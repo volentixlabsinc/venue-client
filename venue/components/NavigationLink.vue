81a74d301d4b35a37f3b8192271fd501cfbccdb7
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-10-04 15:33:35 -0400

    Change lefthand nav text to title case

--- venue/components/NavigationLink.vue
+++ venue/components/NavigationLink.vue
@@ -2,7 +2,7 @@
   <router-link :to="localizedRoute(route, $i18n.locale)" class="navbar-item">
     <div class="level" @click="$emit('click')">
       <span class="icon is-large"><i :class="icon" class="fas fa-lg prefix"/></span>
-      <span class="nav-text is-size-6">{{ name }}</span>
+      <span class="nav-text is-size-5">{{ name }}</span>
     </div>
   </router-link>
 </template>
@@ -36,9 +36,5 @@ export default {
   background-color: #374a59;
   border-left: 5px #84429a solid;
   padding-left: 7px;
-}
-
-.nav-text {
-  text-transform: uppercase;
 }
 </style>
