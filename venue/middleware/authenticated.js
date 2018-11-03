59f7d5420042912df635db6894b6971229d4a4f4
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-11-03 18:06:26 -0400

    Redirect to /login if user tries to hit authenticated page

--- venue/middleware/authenticated.js
+++ venue/middleware/authenticated.js
@@ -1,6 +1,6 @@
 export default function({ store, redirect }) {
   // If the user is not authenticated
   if (!store.state.user.isAuthenticated) {
-    return redirect("/");
+    return redirect("/login");
   }
 }
