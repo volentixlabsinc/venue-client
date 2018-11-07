cb6bd534665962473373f33f5a59d8d0b487e7ed
Shawn Lauzon <shawn.lauzon@gmail.com>
2018-11-07 18:34:54 -0500

    Remove hello function

--- services/googlesheets/handler.js
+++ services/googlesheets/handler.js
@@ -20,19 +20,6 @@ const client = new google.auth.JWT({
   scopes: ["https://www.googleapis.com/auth/spreadsheets"]
 });
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-
 // {
 //   "spreadsheetId": "from-url",
 //   "sheetName": "telegram",
